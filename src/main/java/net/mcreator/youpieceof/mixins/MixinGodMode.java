be0e75e920d3b1cfad51bf8542b31bea778cde68
IveTouchedGrass <109032090+IveTouchedGrass@users.noreply.github.com>
2024-10-22 14:43:46 +0300

    Update MixinGodMode.java

--- src/main/java/net/mcreator/youpieceof/mixins/MixinGodMode.java
+++ src/main/java/net/mcreator/youpieceof/mixins/MixinGodMode.java
@@ -1,2 +1,20 @@
-package net.fathommod.mixins;
+package net.mcreator.youpieceof.mixins;
+
+import net.mcreator.youpieceof.init.FathommodModMobEffects;
+import net.minecraft.world.entity.player.Player;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(Player.class)
+public class MixinElytra {
+    @ModifyReturnValue(method = "hurt", at = @At("RETURN"))
+    public boolean hurt(boolean original) 
+{
+        Player instance = (Player) (Object) this;
+        return instance.getData(FathommodModVariables.PLAYER_VARIABLES).isGodMode ? false : original
+}
+}
+
 
