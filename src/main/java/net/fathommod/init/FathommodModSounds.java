a8c0ed25323995a3f85f4b52bee3e68c88c5243b
IveTouchedGrass <mishkolzii@gmail.com>
2025-01-13 19:00:51 +0200

    Added Ted boss music

--- src/main/java/net/fathommod/init/FathommodModSounds.java
+++ src/main/java/net/fathommod/init/FathommodModSounds.java
@@ -11,9 +11,11 @@ import net.minecraft.sounds.SoundEvent;
 import net.neoforged.neoforge.registries.DeferredHolder;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
+@SuppressWarnings("unused")
 public class FathommodModSounds {
 	public static final DeferredRegister<SoundEvent> REGISTRY = DeferredRegister.create(Registries.SOUND_EVENT, FathommodMod.MOD_ID);
 	public static final DeferredHolder<SoundEvent, SoundEvent> PEPSIMAN = REGISTRY.register("pepsiman", () -> SoundEvent.createVariableRangeEvent(ResourceLocation.fromNamespaceAndPath(FathommodMod.MOD_ID, "pepsiman")));
 	public static final DeferredHolder<SoundEvent, SoundEvent> SMACK = REGISTRY.register("smack", () -> SoundEvent.createVariableRangeEvent(ResourceLocation.fromNamespaceAndPath(FathommodMod.MOD_ID, "smack")));
 	public static final DeferredHolder<SoundEvent, SoundEvent> BONK = REGISTRY.register("bonk", () -> SoundEvent.createVariableRangeEvent(ResourceLocation.fromNamespaceAndPath(FathommodMod.MOD_ID, "bonk")));
+	public static final DeferredHolder<SoundEvent, SoundEvent> TED_BOSS_MUSIC = REGISTRY.register("ted_boss_music", () -> SoundEvent.createVariableRangeEvent(ResourceLocation.fromNamespaceAndPath(FathommodMod.MOD_ID, "ted_boss_music")));
 }
